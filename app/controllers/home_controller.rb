6b19d53c8e8399b1e2c853aadd3d5996303cb249
Anoop Joshi <anoopjoshi@hotmail.co.uk>
2017-09-07 00:05:26 +0100

    ensure date is string to date before using it temporarily reroute all requests to coming soon page apart from ip search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,11 +5,53 @@ class HomeController < ApplicationController
 
   def date_calculator
     @results = {}
-    @results[:date] = params[:date]
-    puts "#{params}"
-    @results[:court] = params[:court]
-    @results[:type] = params[:type]
-    render :results
+    date = params[:date].to_date
+    type = params[:type]
+    court = params[:court]
+    @results = {
+      date: date,
+      type: type,
+      court: court
+    }
+    calculate_dates_for_action(date, type, court)
+    #remove this once all data is in place
+    if type == "Intellectual Property" && court == "Court of Session"
+      render :results
+    else
+      render :coming_soon
+    end
+  end
+
+  def calculate_dates_for_action(date, type, court)
+    case type
+    when "Intellectual Property"
+      intellectual_property(date)
+    when "Commercial"
+      commercial(date)
+    when "Ordinary"
+      ordinary(date)
+    else
+      return  
+    #add remaining ones here when worked out 
+    end
+  end
+
+  def intellectual_property(date)
+    @results[:earliest_calling] = @results[:date] + 21.days #nb this is based on Europe @ no earlier than 21 days - if outside Europe date is likely to be 42 days
+    @results[:defences_due] = @results[:earliest_calling] + 7.days
+    @results[:preliminary_hearing] = @results[:defences_due] + 14.days #within 14 days after defences or answers (as the case may be) have been lodged
+    @results[:intimate_statement_of_proposals] = @results[:preliminary_hearing] - 3.days# not less than 3 days, or such other period as may be prescribed by the judge at the preliminary hearing, before the date fixed under rule 55.2E(4) for the procedural hearing, each party shall lodge in process and send to every other party –
+    @results[:final_calling] = @results[:earliest_calling] + 366.days #a year and a day after expirty of the period of notice so 366 + 21 = 382 days from date of service if in Europe
+  end
+
+  def commercial(date)
+    puts "*******in COMMERCIAL with date as #{date}"
+    return
+  end
+
+  def ordinary(date)
+    puts "*********in ORDINARY with date as #{date}"
+    render :coming_soon
   end
 
 end
